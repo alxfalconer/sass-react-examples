e45d44c50eeb7514be04b93d7eebaf7a13714d3b
nemrosim <nemrosim1988@gmail.com>
2020-09-07 22:58:55 +0300

    small fix

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './1.1.simple-example.scss'
 
 export const App = () => {
-	return (
-		<div>
-			<button>Do something</button>
-		</div>
-	);
+    return (
+        <div>
+            <button>Do something</button>
+        </div>
+    );
 };
