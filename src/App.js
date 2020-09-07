406dd006a7f675a5463496c84da8a4149cb54266
nemrosim <nemrosim1988@gmail.com>
2020-09-07 22:33:00 +0300

    add simple example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './1.1.simple-example.scss'
 
 export const App = () => {
 	return (
 		<div>
-			Hello world!
+			<button>Do something</button>
 		</div>
 	);
 };
