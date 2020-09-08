07e59e490433c728e99861456f9253643fabe928
nemrosim <nemrosim1988@gmail.com>
2020-09-09 01:24:01 +0300

    add new sass examples

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './1.6.extends.scss';
+
+export const App = () => {
+    return (
+        <>
+            <div className='div-with-button'>
+                <button>Do something</button>
+            </div>
+            <div className='div2-with-button'>
+                <button>Do something 2</button>
+            </div>
+        </>
+    );
+};
